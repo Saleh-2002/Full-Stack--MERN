c25de10c7dc0d598b0bd5f14206ad9053f0b1e1a
Saleh Alghool <salehssag40@gmail.com>
2024-07-25 00:00:05 +0300

    Sign Up Page
    sasa

--- CapStone Project/CP Project/index.js
+++ CapStone Project/CP Project/index.js
@@ -15,16 +15,49 @@ app.listen(Port, () => {
     console.log(`Running on Port ${Port}`);
 });
 
-let Users = [
-    { email: "User1", password: "1234" },
-    { email: "User2", password: "1234" }
-];
+let Users = [];
 let MyPosts = [];
 let MyName = null;
 
 app.get("/", (req, res) => {
     res.render("Login", { error: null });
 });
+
+app.post("/Login", (req, res) => {
+    let UserFound = false;
+    const { Name, email, password } = req.body;
+    for (let i = 0; i < Users.length; i++) {
+        if (email.toLowerCase() === Users[i].email.toLowerCase() && password === Users[i].password) {
+            UserFound = true;
+            MyName = Users.Name;
+            res.redirect("/main");
+            break;
+        }
+    }
+    if (!UserFound) {
+        res.render("Login", { error: "Invalid Email or Password!" });
+    }
+});
+
+app.get("/SignUp", (req, res) => {
+    res.render("SignUp", { error: null });
+})
+
+app.post("/SignUp", (req, res) => {
+    try {
+        const { Name, email, password, BirthDay } = req.body;
+        if (Name && email && password && BirthDay) {
+            Users.push({ Name, email, password, BirthDay });
+            res.redirect("/");
+        } else if (Name == null || email == null || password == null || BirthDay == null) {
+            res.render("SignUp", { error: "Please Fill All Fields!" });
+        } else {
+            res.render("SignUp", { error: "Please Fill All Fields!" });
+        }
+    } catch (err) {
+        res.render("SignUp", { error: "Something Went Wrong!" });
+    }
+})
 
 app.get('/update/:id', (req, res) => {
     const id = req.params.id;
@@ -43,21 +76,7 @@ app.get("/main", (req, res) => {
     res.render("MainPage", { MyName, MyPosts, error2: null });
 });
 
-app.post("/Login", (req, res) => {
-    let UserFound = false;
-    const { Name, email, password } = req.body;
-    for (let i = 0; i < Users.length; i++) {
-        if (email.toLowerCase() === Users[i].email.toLowerCase() && password === Users[i].password) {
-            UserFound = true;
-            MyName = Name;
-            res.redirect("/main");
-            break;
-        }
-    }
-    if (!UserFound) {
-        res.render("Login", { error: "Invalid Email or Password!" });
-    }
-});
+
 
 app.post("/Poster", (req, res) => {
     const { Topic, Discription } = req.body;
